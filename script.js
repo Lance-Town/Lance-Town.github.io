7077e019af868a6ecac3c92bd3aad35b873a9197
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2023-07-04 11:36:06 -0700

    Add section scrolling animation
    - Add hidden tag to sections that will animate on scroll
    - Rearranged JS code to be encapsulated in functions

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Get the navbar
-var navbar = document.querySelector(".navContainer");
-
-var sticky = navbar.offsetTop;
-
-
-function stickynavbar() {
-  console.log(sticky);
-  if (document.documentElement.scrollTop >= sticky) {    
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");    
-  }
+function initStickyNavBar() {
+  // Get the navbar
+  const navbar = document.querySelector(".navContainer");
+  const sticky = navbar.offsetTop;
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop >= sticky) {    
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");    
+    }
+  });
 }
-window.addEventListener("scroll", stickynavbar);
+
+function animateElements() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+}
+
+initStickyNavBar();
+animateElements();
