cd4b68fafafea068c1e373864d09bab26da21e64
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2023-07-05 14:51:29 -0700

    Add smooth scroll
    - Add smooth scrolling function that defaults to scroll to top of page

--- script.js
+++ script.js
@@ -24,5 +24,29 @@ function animateElements() {
   hiddenElements.forEach((el) => observer.observe(el));
 }
 
+// smooth scrolling to any element, defaults to the top
+function scrollToElement(myElement = "#introductionContainer", scrollDuration = 500) {
+  const elementExists = document.querySelector(myElement);
+  if (elementExists && elementExists.getBoundingClientRect) {
+      const rect = elementExists.getBoundingClientRect();
+      const elementTop = rect.top + window.scrollY - 200; // a bit of space from top
+      var cosParameter = (window.scrollY - elementTop) / 2,
+          scrollCount = 0,
+          oldTimestamp = performance.now();
+      function step(newTimestamp) {
+          console.log(scrollCount);
+          scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
+          if (scrollCount >= Math.PI) {
+              window.scrollTo(0, elementTop);
+              return;
+          }
+          window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)) + elementTop);
+          oldTimestamp = newTimestamp;
+          window.requestAnimationFrame(step);
+      }
+      window.requestAnimationFrame(step);
+  }
+}
+
 initStickyNavBar();
 animateElements();
